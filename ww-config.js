47df5848ca50d7bddb28d370de827bf59114be60
luka-weweb <luka@weweb.io>
2024-04-29 11:11:31 +0200

    a

--- ww-config.js
+++ ww-config.js
@@ -8,13 +8,10 @@ export default {
       groups: ["Trigger", "Dropdown"],
     },
   },
-  actions: [{ label: 'Close dropdown', action: 'closeDropdown' },
-  { label: 'Open dropdown', action: 'openDropdown' }],
   properties: {
     name: {
       type: 'Text',
       label: 'Name',
-      settings: true,
       bindable: true,
       defaultValue: "mdi-light:home"
     },
