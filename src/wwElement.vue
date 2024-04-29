121a2f774521f4dfd2ff9a69042a4ec74ab13fd9
luka-weweb <luka@weweb.io>
2024-04-29 11:05:52 +0200

    a

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -5,6 +5,7 @@ import { Icon } from '@iconify/vue';
 <template>
   <div style="position: relative;">
     <Icon icon="mdi-light:home" />
+    <Icon :icon="this.content.name" />
 </div>
 </template>
 
